ea818afdea978684c676818d5e868b6f60bb7f44
Barath <barath6.99@gmail.com>
2020-06-22 13:45:39 +0530

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import Menu from './MenuComponent';
 import ContactUs from './ContactComponent';
 import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import Reservation from './ReservationComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -68,6 +69,22 @@ const HomeNavigator = createStackNavigator({
                 onPress={() => navigation.toggleDrawer()} />
         })
     });
+    const ReservationNavigator = createStackNavigator({
+        Reservation: { screen: Reservation }
+      }, {
+        navigationOptions: ({ navigation }) => ({
+          headerStyle: {
+              backgroundColor: "#512DA8"
+          },
+          headerTitleStyle: {
+              color: "#fff"            
+          },
+          headerTintColor: "#fff",
+          headerLeft: <Icon name="menu" size={24}
+            iconStyle={{ color: 'white' }} 
+            onPress={ () => navigation.navigate('DrawerToggle') } />    
+        })
+      })
 
 const ContactNavigator = createStackNavigator({
     Contact: { screen: ContactUs }
@@ -168,6 +185,21 @@ const MainNavigator = createDrawerNavigator({
             )
         },
     },
+    Reservation:
+      { screen: ReservationNavigator,
+        navigationOptions: {
+          title: 'Reserve Table',
+          drawerLabel: 'Reserve Table',
+          drawerIcon: ({ tintColor, focused }) => (
+            <Icon
+              name='cutlery'
+              type='font-awesome'            
+              size={24}
+              iconStyle={{ color: tintColor }}
+            />
+          ),
+        }
+      },
     Contact:
     {
         screen: ContactNavigator,
