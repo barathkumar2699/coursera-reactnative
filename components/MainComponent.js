41816815eb30f6b2b17b1b5b01ed91d901ec94da
Barath <barath6.99@gmail.com>
2020-06-22 12:27:55 +0530

    Using Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -8,7 +8,25 @@ import { DISHES } from '../shared/dishes';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 
+
+const mapStateToProps = state => {
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      promotions: state.promotions,
+      leaders: state.leaders
+    }
+  }
+  const mapDispatchToProps = dispatch => ({
+    fetchDishes: () => dispatch(fetchDishes()),
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos()),
+    fetchLeaders: () => dispatch(fetchLeaders()),
+  })
+  
 const MenuNavigator = createStackNavigator({
     Menu: {
         screen: Menu,
@@ -172,6 +190,13 @@ const MainNavigator = createDrawerNavigator({
     });
 
 class Main extends Component {
+    componentDidMount() {
+        this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
+        this.props.fetchLeaders();
+      }
+      
     constructor(props) {
         super(props);
         this.state = {
@@ -216,4 +241,4 @@ const styles = StyleSheet.create({
     }
   });
 
-export default Main;
+  export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,3 @@
 export const baseUrl = 'http://192.168.43.195:3001/'
+
+//json-server --host 0.0.0.0 --port 8000 ./db.json --watch
