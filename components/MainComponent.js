7a9631a15cc8858327f5a7d718afb4218114b09b
Barath <barath6.99@gmail.com>
2020-06-27 13:43:17 +0530

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,6 +7,7 @@ import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -28,7 +29,22 @@ const mapStateToProps = state => {
     fetchPromos: () => dispatch(fetchPromos()),
     fetchLeaders: () => dispatch(fetchLeaders()),
   })
-  
+  const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+  }, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.toggleDrawer() } />    
+  })
+});
 const MenuNavigator = createStackNavigator({
     Menu: {
         screen: Menu,
@@ -154,6 +170,21 @@ const CustomDrawerContentComponent = (props) => (
 );
 
 const MainNavigator = createDrawerNavigator({
+    Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
     Home:
     {
         screen: HomeNavigator,
@@ -250,6 +281,7 @@ const MainNavigator = createDrawerNavigator({
         }
       },
 }, {
+    initialRouteName:'Home',
     drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
     });
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://192.168.43.100:3001/'
+export const baseUrl = 'http://192.168.43.195:3001/'
 
 //json-server --host 192.168.43.195 --port 3001 ./db.json --watch
